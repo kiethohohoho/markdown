9c065320dd6dfb1cbe984d42d73ae887c01d6c73
Lee Gusion <1951120099@sv.ut.edu.vn>
2022-03-23 16:27:53 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
 
-function App() {
+function Markdown() {
+  const mdParser = new MarkdownIt(/* Markdown-it options */);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MdEditor
+      style={{ height: "500px" }}
+      renderHTML={(text) => mdParser.render(text)}
+    />
   );
 }
-
-export default App;
+export default Markdown;
